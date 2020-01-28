d1dc887b8fb8eb8299f9efc77048f474bfbcf0bf
masato.uesaka <ec2-user@ip-172-31-89-13.ec2.internal>
2020-01-28 09:00:54 +0000

    コレクトユーザーで他のユーザーを除外

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
  before_action :require_user_logged_in
- 
+ before_action :correct_user, except: [:index]
  before_action :set_task, only: [:show, :edit, :update, :destroy]
   
   def index
@@ -65,5 +65,12 @@ class TasksController < ApplicationController
   # Strong Parameter
   def task_params
     params.require(:task).permit(:content, :status)
+  end
+   
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
   end
 end
